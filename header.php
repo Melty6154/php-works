98ad40d8df23c5259fb8c840545566840fa24898
Melty6154 <Mahziyartvkli@gmail.com>
2024-08-20 17:23:07 +0330

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-    <html>
-        <h1>The most successful clade of snakes, Colubroides.</h1>
-        <p>
-            <?php
-                echo "The only clade of snakes to not possess legs, Colubroids are the most successful clade of snakes on earth. Made up of over 85% of all snake species on earth, they can be found everywhere on earth except for antarctica";
-            ?>
-        </p>
-    </html>            
